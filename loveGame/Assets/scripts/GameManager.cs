367ab14c7841bb2bc9a165dd917b09a65f99b943
Cooper <cooper.howling@gmail.com>
2017-10-08 22:42:19 -0700

    added main menu play button function, and initial backstory scene functionality minus the actual conversation of backstory

---
+++ loveGame/Assets/scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Conversation[] conversations;
+    [SerializeField]
+    private Text convo;
+    public int i;
+
+    public void nextSpeech()
+    {
+        convo.text = conversations[i++].speech;
+    }
+
+    void Start()
+    {
+        i = 0;
+        convo.text = conversations[i++].speech;
+  
+    }
+}
---
+++ loveGame/Assets/scripts/conversation1.cs
@@ -0,0 +1,6 @@
+﻿
+[System.Serializable]
+public class Conversation
+{
+    public string speech;
+}
