276f79a3ece37e186c398cf874009a9798a033a1
Matt <mccormackm14@gmail.com>
2017-10-16 00:18:52 -0700

    Added level 1

---
+++ loveGame/Assets/scripts/changeRhyme.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeRhyme : MonoBehaviour {
+
+
+    public Text rhymeText = null;
+
+    public void cRhyme(string rhyme)
+    {
+        rhymeText.text = rhyme;
+        
+
+    }
+
+}
